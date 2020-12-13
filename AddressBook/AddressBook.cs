56b7c35022a14b866cc6ec9adb1dd3062c7314e9
jkchuong <jkchuong@gmail.com>
2020-12-13 13:18:05 +0000

    Add project files.

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        static void Main(string[] args)
+        {
+            Entry entry1 = new Entry("Jimmy", "Chuong");
+            entry1.Address = "Portcullis House";
+            entry1.Email = "veryimportantperson@hotmail.co.uk";
+            entry1.Phone = 07264742776;
+            Console.WriteLine(entry1.GetAll());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AddressBook/Entry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class Entry
+    {
+        // Constructor
+        public Entry(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+
+        public string GetAll()
+        {
+            return $"First Name: {FirstName} \nLast Name: {LastName}" +
+                $"\nAddress: {Address} \nEmail: {Email} \n" +
+                $"Phone: {Phone.ToString("D11")}";
+        }
+    }
+}
